86298e90608dd0983a8379b165cf7278919132f4
Woidless <bmoblp@mail.ru>
2023-09-25 17:59:29 +0600

    111

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+
+bot = telebot.TeleBot('6543463555:AAEtz4zegf0TK6oJluePPMG9KJqitmuJtvo')
+
+# Словарь для хранения сообщений и каналов
+channels = ['@polezpolez']
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Приветствую {message.from_user.first_name}  {message.from_user.last_name} \nОтправьте вашу анкету одним следующим сообщением \nимя фамилия возраст специальность '
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+    bot.send_message('@polezpolez', 'hello')
+
+@bot.message_handler(func=lambda message:True)
+def handle_message(message):
+    user_message = message.text.split()
+    for channel in channels:
+        bot.send_message(channel, f'Имя = {user_message[0]} \nФамилия =  {user_message[1]} \nИмя = {user_message[2]} \nИмя = {user_message[3]} \n')
+    bot.send_message(message.chat.id, 'Ваша анкета отправлена по группам', parse_mode='html')
+
+bot.polling(none_stop=True)
